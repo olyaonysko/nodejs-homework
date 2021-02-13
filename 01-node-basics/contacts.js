c1f4838aff91936073abbd0eb21a5e730492305c
Olya <olya.onysko@gmail.com>
2021-02-13 17:41:10 +0200

    fix

--- 01-node-basics/contacts.js
+++ 01-node-basics/contacts.js
@@ -1,6 +1,5 @@
 import * as fs from "fs/promises";
 import * as path from "path";
-import shortid from "shortid";
 
 import getDirname from "./helpers/dirname.js";
 import { handleError } from "./helpers/handleerror.js";
